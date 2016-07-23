60c9d7b3cdb7af304d62730bc426172036320941
Alan <wangchaoallan@gmail.com>
2016-07-23 14:42:35 +1000

    Add clear grid function
    Be very careful, onclick = "clear()" does not work, because clear must
    be a pre existing function of Javascript libaray.  Spent at least 30
    minute to figure it out.

--- sketchpad.js
+++ sketchpad.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 
 	
-    for(var i = 0; i<256;i++){
+    for(var i = 0; i<256;i++)
 	$('.wrapper').append("<div></div>");
+
 	$('.wrapper').find('div').addClass('grid');
-}	$('.wrapper').find('div').on('mouseenter',function(){
-	$(this).toggleClass('highlight');
+	$('.wrapper').find('div').on('mouseenter',function(){
+	$(this).addClass('highlight');
 
 });
+
 });
+
+	function toClear(){
+	
+	$('.wrapper').find('div').removeClass('highlight');
+}
+
