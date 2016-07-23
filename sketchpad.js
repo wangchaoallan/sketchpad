d110ba27c3cd8212f429d2035986483d6897c62f
Alan <wangchaoallan@gmail.com>
2016-07-23 13:21:10 +1000

    Change colour
    The colour changes when we hover /leave the grid

--- sketchpad.js
+++ sketchpad.js
@@ -4,5 +4,8 @@ $(document).ready(function(){
     for(var i = 0; i<256;i++){
 	$('.wrapper').append("<div></div>");
 	$('.wrapper').find('div').addClass('grid');
-}
+}	$('.wrapper').find('div').on('mouseenter',function(){
+	$(this).toggleClass('highlight');
+
+});
 });
