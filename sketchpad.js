28aaacf015c817a17236e9b715996ceebd2c74a7
Chao Wang <wangchaoalan@gmail.com>
2016-07-24 23:46:10 +1000

    Successfully change width and height
    Successfully change width and height danamicly,

--- sketchpad.js
+++ sketchpad.js
@@ -1,19 +1,34 @@
+
+var input = 64;
+
+
 $(document).ready(function(){
 
 	
-    for(var i = 0; i<256;i++)
-	$('.wrapper').append("<div></div>");
+	for(var i = 0; i<input*input;i++){
+		$('.wrapper').append("<div></div>");
+	}
 
-	$('.wrapper').find('div').addClass('grid');
-	$('.wrapper').find('div').on('mouseenter',function(){
-	$(this).addClass('highlight');
+	var grid = $('.wrapper').find('div');
+
+	grid.width(900/input);
+	grid.height(900/input);
+	$('.wrapper').width(900+input*4);
+	$('.wrapper').height(900+input*4);
+	grid.addClass('gridStyle');
+	grid.on('mouseenter',function(){
+		$(this).addClass('highlight');
+
+	});
 
 });
 
-});
-
-	function toClear(){
-	
+function toClear(){
 	$('.wrapper').find('div').removeClass('highlight');
 }
 
+/*	function newGrid(){
+
+		 input = prompt("How many grids per line do you wish to set?");
+
+		}*/
