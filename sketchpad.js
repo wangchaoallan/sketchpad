4cb8c46edbc9fab47e7f5c1478714ead3dc03834
Chao Wang <wangchaoalan@gmail.com>
2016-07-24 23:12:52 +1000

    atempt to danamicly change height and width, failed
    atempt to danamicly change height and width, failed

--- sketchpad.js
+++ sketchpad.js
@@ -1,9 +1,22 @@
+
+var input = 16;
+var test = 16;
+
 $(document).ready(function(){
 
 	
-    for(var i = 0; i<256;i++)
+    for(var i = 0; i<input;i++){
 	$('.wrapper').append("<div></div>");
+   }
 
+	$('.wrapper').find('div').css({
+    width: function(test) {
+      return 900/test;
+    },
+    height: function(test) {
+     return 900/test;
+    }
+  });
 	$('.wrapper').find('div').addClass('grid');
 	$('.wrapper').find('div').on('mouseenter',function(){
 	$(this).addClass('highlight');
@@ -13,7 +26,11 @@ $(document).ready(function(){
 });
 
 	function toClear(){
-	
 	$('.wrapper').find('div').removeClass('highlight');
 }
 
+/*	function newGrid(){
+
+		 input = prompt("How many grids per line do you wish to set?");
+
+	}*/
